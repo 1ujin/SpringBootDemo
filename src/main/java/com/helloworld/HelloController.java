f25f41f28070ef10e6d5c8cc6d0c386a2a5056fb
lujin <lujin.vip@foxmail.com>
2019-04-29 22:05:43 +0800

    Spring Boot 练习

---
+++ src/main/java/com/helloworld/HelloController.java
@@ -0,0 +1,20 @@
+package com.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${com.didispace.blog.name}")
+    private String name;
+    @Value("${com.didispace.blog.title}")
+    private String title;
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World<br>" + name + "<br>" + title;
+    }
+
+}
