c96547747af258242023f49ce95b44c1321ca1d7
lujin <lujin.vip@foxmail.com>
2019-05-02 03:16:40 +0800

    Delete unused imports.

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -2,9 +2,6 @@ package com.controller;
 
 import com.domain.User;
 import org.springframework.web.bind.annotation.*;
-
-import javax.jws.soap.SOAPBinding;
-import javax.websocket.server.PathParam;
 import java.util.*;
 
 @RestController
