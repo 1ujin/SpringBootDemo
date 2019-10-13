f2de36cf7b739e48d9abb94b5454c0660934905e
lujin <lujin.vip@foxmail.com>
2019-10-14 03:51:22 +0800

    add new class

---
+++ src/main/java/com/controller/FileUploadController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    // 可以使用一个POJO来包装RequestParam所注释的参数，这个POJO本身没有要求额外的注解，但是POJO本身必须包含和请求参数完全匹配的字段，标准的setter/getter，和一个无参的构造器 https://www.jianshu.com/p/b4c8d8de4dad
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return "Upload successfully.";
+    }
+}
