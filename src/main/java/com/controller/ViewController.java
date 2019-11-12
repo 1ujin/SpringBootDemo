33e6959e682be797f71d9f438926a08eec985b15
lujin <lujin.vip@foxmail.com>
2019-11-13 01:44:10 +0800

    internationalization

--- src/main/java/com/controller/ViewController.java
+++ src/main/java/com/controller/ViewController.java
@@ -1,12 +1,20 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping(value = "/view-users")
     public String viewUsers() {
+        System.out.println(messageSource.getMessage("welcome.text", null, LocaleContextHolder.getLocale()));
         return "view-users";
     }
 
