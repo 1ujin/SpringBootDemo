09d2dcfa5020ce6b58c121a175e614e7e12f5832
lujin <lujin.vip@foxmail.com>
2019-11-15 17:24:16 +0800

    Swagger2添加文档内容

--- src/main/java/com/controller/SwaggerAPIController.java
+++ src/main/java/com/controller/SwaggerAPIController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
 import com.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,7 +16,9 @@ public class SwaggerAPIController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/api/user")
+    @ApiOperation(value="获取用户列表", notes="返回全部用户，容器为数组。")
+    @ApiImplicitParam(name = "无", required = false, dataType = "User")
+    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
     public List<Object> getUsers() {
         return new ArrayList<>(userService.getUsers());
     }
